cc09966e3c11098387388d9bccd74ca405617abe
EdFage <87755165+EdFage@users.noreply.github.com>
2023-11-28 12:15:40 +0000

    fix error in request

--- service_worker.js
+++ service_worker.js
@@ -21,33 +21,36 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
                 const endpoint = "https://api.openai.com/v1/chat/completions";
 
                 const model = "gpt-3.5-turbo";
-                const prompt = `"${info.selectionText}"`
-                const message = [{role: "system", content: "You create google calendar URLs from text. In all your responses, return only the google calendar url."}, {content: prompt}];
+                const prompt = `${info.selectionText}`
                 const max_tokens = 256;
-
-                fetch(endpoint, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": `Bearer ${apiKey}`
-                        },
-                        body: JSON.stringify({
-                            model: model,
-                            messages: [message],
-                            max_tokens: max_tokens
-                        })
+                const messages = [
+                    {
+                      role: "system",
+                      content: "You create google calendar URLs from text. In all your responses, return only the google calendar url."
+                    },
+                    {
+                      role: "user",
+                      content: prompt
+                    }
+                  ];
+                  
+                  fetch(endpoint, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      "Authorization": `Bearer ${apiKey}`
+                    },
+                    body: JSON.stringify({
+                      model: model,
+                      messages: messages, // corrected line
+                      max_tokens: max_tokens
                     })
-                    .then(response => response.json())
-                    .then(data => {
-                        const calendarURL = data.choices[0].message.content;
-                        chrome.scripting.insertCSS({
-                            target: { tabId: tab.id },
-                            css: 'body { cursor: default; }'
-                        });
-                        chrome.tabs.create({
-                            url: calendarURL
-                        });
-                    });
+                  })
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                  });
+                    
             }
         });
     }
